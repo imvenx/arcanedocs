adcbfb7de66fa457c282615c0389bcb5d073486c
imvenx <imvenx@proton.me>
2024-01-22 10:31:58 +0100

    improve comments

--- guide/godot4/get-started-assets/Main.gd
+++ guide/godot4/get-started-assets/Main.gd
@@ -10,7 +10,7 @@ func _ready():
 # INFO: This is a trick we do to have automatically loaded the game pad in the web exports
 # The way it works is: We set the export WebGL without the View scene, so when we load on our
 # phone, it will go automatically to the Pad scene, but on the editor, since it has all the
-# scenes it will load the View. 
+# scenes it will load the View. Feel free to change it to fit your workflow.
 func goToPropperScene():
     if ResourceLoader.exists(PadScenePath) and not ResourceLoader.exists(ViewScenePath):
         goToPadScene()
